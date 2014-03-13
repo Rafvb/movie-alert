b89846685b24f72136e77f4048b747bbc3faca72
Raf Van Baelen <rafvanbaelen@gmail.com>
2014-03-13 21:33:43 +0100

    Initial commit

---
+++ client/src/app/app.js
@@ -0,0 +1,10 @@
+angular.module('app');
+
+angular.module('app').constant('MONGOLAB_CONFIG', {
+  baseUrl: '/databases/',
+  dbName: 'ascrum'
+});
+
+angular.module('app').controller('AppCtrl', ['$scope', function($scope) {
+
+}]);
