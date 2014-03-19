0b4adaf8c63d63d575f905433ac3107f9fe8485d
Raf Van Baelen <rafvanbaelen@gmail.com>
2014-03-19 14:32:24 +0100

    Enabled HTML5 mode on client and server

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -1,5 +1,9 @@
 var app = angular.module('app', []);
 
+app.config(['$locationProvider', function($locationProvider) {
+  $locationProvider.html5Mode(true);
+}]);
+
 app.controller('AppCtrl', function($scope) {
   $scope.userName = "Raf";
   $scope.movies = ["The Godfather", "The Shining"];
--- server/server.js
+++ server/server.js
@@ -15,6 +15,8 @@ app.use(express.logger());                                  // Log requests to t
 // A standard error handler - it picks up any left over errors and returns a nicely formatted server 500 error
 app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 
+require('./lib/routes/appFile').addRoutes(app, config);
+
 // Start up the server on the port specified in the config
 server.listen(config.server.listenPort, '0.0.0.0', 511, function() {
   // // Once the server is listening we automatically open up a browser
